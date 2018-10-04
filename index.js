ad52e08bcfd3bfb962f6c2c9f1686b9844c67eec
Morgan ORourke <mporourke22@gmail.com>
2018-10-04 18:41:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ var funkyFunction = function() {
   }
 }
 
-console.log(funkyFunction)
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
@@ -28,3 +27,5 @@ console.log(funkyFunction)
 var theFunk = function(){
   return funkyFunction
 }
+
+console.log(theFunk)
